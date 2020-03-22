23473d38c313b330508e80680a7bb481d4529aa5
Alex <dnjscksdn98@gmail.com>
2020-03-22 15:38:12 +0900

    #2.1 create profile component

---
+++ src/containers/ProfileContainer.js
@@ -0,0 +1,8 @@
+import React, { useEffect } from "react";
+import Profile from "../components/Profile";
+
+function ProfileContainer() {
+  return <Profile />;
+}
+
+export default ProfileContainer;
